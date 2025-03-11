72f2674c7e986eaacf846d88164b5876f849b1f4
muuo-dev <muuojoel210@gmail.com>
2025-03-11 15:55:10 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.1)';
+    });
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
