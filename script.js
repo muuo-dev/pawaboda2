11d1a903574cdb0667ccbf394b79ed1e4f99e3d4
muuo-dev <muuojoel210@gmail.com>
2025-03-12 13:57:49 +0300

    Updated website

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-document.querySelectorAll('nav a').forEach(button => {
-    button.addEventListener('mouseover', () => {
-        button.style.transform = 'scale(1.1)';
-    });
-    button.addEventListener('mouseout', () => {
-        button.style.transform = 'scale(1)';
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".fade-in").forEach(element => {
+        element.classList.add("visible");
     });
 });
